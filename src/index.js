2a64febd54756219dd7534f21189d72162545e69
Rosa Marquez <rmarquez101@cnm.edu>
2021-11-16 03:53:07 -0700

    updated menu

--- src/index.js
+++ src/index.js
@@ -2,22 +2,23 @@ const express = require("express")
 const morgan = require("morgan")
 const bodyParser = require("body-parser")
 const Recaptcha = require("express-recaptcha").RecaptchaV2
-const formData = require ("form-data")
-const Mailgun = require ("mailgun.js")
+const formData = require("form-data")
+//https://www.npmjs.com/package/mailgun.js
+const Mailgun = require("mailgun.js")
 const mailgun = new Mailgun(formData)
-const {check, validationResult} = require("express-validator")
-const {request, response} = require("express");
 
-const Validation = [
+const {check, validationResult} = require("express-validator")
+
+const validation = [
     check("name", "A valid name is required.").not().isEmpty().trim().escape(),
     check("email", "Please provide a valid email").isEmail(),
     check("subject").optional().trim().escape(),
-    check("message", "A message of 2000 characters is required").trim().escape().isLength{min:1,max:2000},
+    check("message", "A message shorter than 2000 characters is required").trim().escape().isLength({min:1, max:2000})
 ]
 
 const app = express()
 const recaptcha = new Recaptcha(process.env.RECAPTCHA_SITE_KEY, process.env.RECAPTCHA_SECRET_KEY)
-const mg= mailgun.client({username:'api', key: process.env.MAILGUN_API_KEY})
+const mg = mailgun.client({username: 'api', key: process.env.MAILGUN_API_KEY})
 
 app.use(morgan("dev"))
 app.use(express.json())
@@ -26,27 +27,28 @@ app.use(bodyParser.json())
 
 const indexRoute = express.Router()
 
-const handleGetRequest = (request, response) => {
-  return response.json("The express server is live")
+const handleGetRequest = (req, res) => {
+    return res.json("The express server is live 😼")
 }
 
-const handlePostRequest = (request, response) => {
-    response.append('Content-Type', 'text/html')
+const handlePostRequest = (req, res) => {
+    res.append('Content-Type', 'text/html')
 
-    if(request.recaptcha.error){
-        request response.send(
-            `<div class='alert alert-danger' role='alert'><strong>Oh Snap!</strong> There was a recaptcha error. Please try again</div>`
-        )
-    }
+    // if (req.recaptcha.error) {
+    //   return response.send(
+    //     `<div class='alert alert-danger' role='alert'><strong>Oh snap!</strong>There was an error with Recaptcha please try again</div>`
+    //   );
+    // }
 
-    const errors = validationResult(request)
+    const errors = validationResult(req)
 
-    if(errors.isEmpty() === false){
+    if(errors.isEmpty() === false) {
         const currentError = errors.array()[0]
-        return response.send(`<div class='alert alert-danger' role='alert'><strong>Oh Snap!</strong> ${currentError}</div>\``)
+
+        return res.send(`<div class="alert alert-danger" role="alert"><strong>Oh snap!</strong> ${currentError.msg}</div>`)
     }
 
-    const {email, subject, name, message} = request.body
+    const {email, subject, name, message} = req.body
 
     const mailgunData = {
         to: process.env.MAIL_RECIPIENT,
@@ -57,23 +59,23 @@ const handlePostRequest = (request, response) => {
 
     mg.messages.create(process.env.MAILGUN_DOMAIN, mailgunData)
         .then(msg =>
-        response.send(
-            `<div class='alert alert-success' role='alert'>${json.stringify(msg)} </div>`
-        ))
-        .catch(err=>
-        response.send(
-            `<div class='alert alert-danger' role='alert'><strong>Oh Snap!</strong> ${err}</div>`
-        ))
+            res.send(
+                `<div class='alert alert-success' role='alert'>${JSON.stringify(msg)}</div>`
+            )
+        )
+        .catch(err =>
+            res.send(
+                `<div class="alert alert-danger" role="alert"><strong>Oh snap!cfc</strong>${err}</div>`
+            )
+        )
 }
 
 indexRoute.route("/")
     .get(handleGetRequest)
-    .post(recaptcha.middleware.verify, validation,handlePostRequest)
+    .post(recaptcha.middleware.verify, validation, handlePostRequest)
 
-app.use('/apis', indexRoute)
+app.use("/apis",indexRoute)
 
 app.listen(4200, () => {
-    console.log('Express Successfully Built')
-
-    }
+    console.log("Express Successfully built")
 })
